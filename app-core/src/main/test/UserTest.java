70454562f33c4c754724f06e505c2928f8466848
Vlad <Mr.Straj@yandex.ru>
2014-10-19 21:18:51 +0300

    pish2

---
+++ app-core/src/main/test/UserTest.java
@@ -0,0 +1,21 @@
+package com.epam.brest.courses.domain;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+public class UserTest {
+    User user;
+    @Before
+    public void setUp() throws Exception {
+        user = new User();
+    }
+    @Test
+    public void testGetName() throws Exception {
+        user.setName("User Name");
+        assertEquals("User Name", user.getName());
+    }
+    @Test
+    public void testGetLogin() throws Exception {
+        user.setLogin("User Login");
+        assertEquals("User Login", user.getLogin());
+    }
+}
