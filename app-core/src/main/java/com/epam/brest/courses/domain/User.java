529e2fc859f44f62f0fd2f659eaff92820d4e19f
Vlad <Mr.Straj@yandex.ru>
2014-10-19 21:26:44 +0300

    pish4

--- app-core/src/main/java/com/epam/brest/courses/domain/User.java
+++ app-core/src/main/java/com/epam/brest/courses/domain/User.java
@@ -1,24 +1,28 @@
 package com.epam.brest.courses.domain;
+
+/**
+ * Created by vlad on 19.10.14.
+ */
 public class User {
-private Long userId;
-private String login;
-private String name;
-public Long getUserId() {
-return userId;
-}
-public void setUserId(Long userId) {
-this.userId = userId;
-}
-public String getLogin() {
-return login;
-}
-public void setLogin(String login) {
-this.login = login;
-}
-public String getName() {
-return name;
-}
-public void setName(String name) {
-this.name = name;
-}
+    private Long userId;
+    private String login;
+    private String name;
+    public Long getUserId() {
+        return userId;
+    }
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+    public String getLogin() {
+        return login;
+    }
+    public void setLogin(String login) {
+        this.login = login;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
 }
