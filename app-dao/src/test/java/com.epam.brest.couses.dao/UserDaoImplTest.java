7e1dddd7457fa42a5e12ba0c8404b5d94ccdc83c
Vlad <Mr.Straj@yandex.ru>
2014-10-24 12:59:35 +0300

    Ошибка загрузки ApplicationContext

--- app-dao/src/test/java/com.epam.brest.couses.dao/UserDaoImplTest.java
+++ app-dao/src/test/java/com.epam.brest.couses.dao/UserDaoImplTest.java
@@ -7,9 +7,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import java.util.List;
 import static org.junit.Assert.*;
-
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "classpath:/testApplicationContextSpring.xml" })
+@ContextConfiguration(locations = { "classpath: /testApplicationContextSpring.xml" })
 public class UserDaoImplTest {
     @Autowired
     private UserDao userDao;
